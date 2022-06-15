366ab8374e52fccecd63dc2ce770e220ad5cc43c
apprenant55 <digital2dreamer@gmail.com>
2022-06-15 15:11:15 +0200

    Add VStack / add TabBar

--- GitProject/ContentView.swift
+++ GitProject/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Git lovers!")
-            .padding()
+        VStack {
+            Text("Hello, Git lovers!")
+                .padding()
+            Text("What a wonderful day !")
+        }
     }
 }
 
