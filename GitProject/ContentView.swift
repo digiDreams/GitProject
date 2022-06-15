9c63acc1ebf03a9bccf87806c44a0ddfa2f9e245
apprenant55 <digital2dreamer@gmail.com>
2022-06-15 14:54:44 +0200

    Second commit

---
+++ GitProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitProject
+//
+//  Created by apprenant55 on 15/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Git lovers!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by apprenant55 on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
